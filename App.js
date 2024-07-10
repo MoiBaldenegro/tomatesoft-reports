ab4739eb9fbbe9b0143a288b9a24c43fc5c5bab2
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2024-07-10 14:17:51 -0600

    chore: :white_check_mark: first commit
    first commit
    
    first commit

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [reports, setReports] = useState([]);
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button
+        title="Click me"
+        onPress={() => alert("Button Clicked")}
+        color={"red"}
+      />
+      <TouchableHighlight>
+        <Text>Touchable Highlight</Text>
+      </TouchableHighlight>
     </View>
   );
 }
@@ -13,8 +29,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
